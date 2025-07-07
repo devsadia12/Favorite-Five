d57dee254f18d7e4888a417a4e516cdf501cf96c
devsadia12 <Afrosesadia75@gmail.com>
2025-07-07 23:39:02 +0600

    done

---
+++ scripts.js
@@ -0,0 +1,56 @@
+
+let AlexOvechkin = {
+    name: "Alex Ovechkin",
+    imgUrl: "img/hockeypl1.jpeg"
+    btnid: "alex"
+};
+
+let AustonMathews = {
+    name: "Auston Mathews",
+    imgUrl: "img/hockeypl2.jpeg"
+    btnid: "auston"
+};
+
+let Staneley = {
+    name: "Staneley",
+    imgUrl: "img/hockeypl3.png"
+    btnid: "staneley"
+};
+
+let SideneyCrosby = {
+    name: "Sideney Crosby",
+    imgUrl: "img/hockeypl4.jpeg"
+    btnid: "sideney"
+};
+
+let ConnorMcDavid = {
+    name: "Connor McDavid",
+    imgUrl: "img/hockeypl5.jpg"
+    btnid: "connor"
+};
+
+let VictorHedman = {
+    name: "Victor Hedman",
+    imgUrl: "img/hockeypl6.webp"
+    btnid: "victor"
+};
+
+let Patrik = {
+    name: "Patrik",
+    imgUrl: "img/hockeypl7.jpg"
+    btnid: "patrik"
+};
+
+let NikitaKucherov = {
+    name: "Nikita Kucherov",
+    imgUrl: "img/hockeypl8.png"
+    btnid: "nikita"
+};
+
+let MarioLemieux = {
+    name: "Mario Lemieux",
+    imgUrl: "img/hockeypl9.webp"
+    btnid: "mario"
+};
+
+
